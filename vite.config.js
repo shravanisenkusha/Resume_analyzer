310ac2e9480a0fe0aa99c58cdf2250dc677df120
shravanisenkusha <shravanip@senkusha.in>
2025-08-09 19:49:20 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Resume_analyzer/', // 👈 important for GitHub Pages
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  base: "/", // keep as "/" if repo is username.github.io, else '/repo-name/'
 })
