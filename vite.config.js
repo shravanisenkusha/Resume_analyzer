5abd6319200e6a0ad0f144047136077c73a5944e
shravanisenkusha <shravanip@senkusha.in>
2025-08-09 20:32:56 +0530

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // keep as "/" if repo is username.github.io, else '/repo-name/'
-})
+  base: "/Resume_analyzer/",
+});
